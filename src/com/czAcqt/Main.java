e70059e86736d8abd5d7202ddbae165f348d872d
softwareCQT <softwareCQT@users.noreply.github.com>
2020-04-03 09:27:06 +0800

    初始化成功

---
+++ src/com/czAcqt/Main.java
@@ -0,0 +1,17 @@
+package com.czAcqt;
+
+/***
+ * @author chenqiting
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //TODO  检查参数，并检索
+
+        //TODO 根据参数开始生成表达式
+
+        //TODO 表达式生成结果返回，以Map<String, String>的question，answer形式
+
+        //TODO 写入文件
+    }
+}
---
+++ src/com/czAcqt/generate/calculate.java
@@ -0,0 +1,9 @@
+package com.czAcqt.generate;
+
+/**
+ * @author chenqiting
+ * 计算的类，主要通过后缀表达式来计算结果
+ */
+public class calculate {
+
+}
